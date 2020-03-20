3608b8e1562615b72b8526d3d0e0bba4dda99a8e
webtheory@mail.ru <andromeda.060316698>
2020-03-20 15:55:12 +0200

    update avatar

--- src/components/Avatar/Avatar.js
+++ src/components/Avatar/Avatar.js
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import classNames from 'classnames/bind'
 import getMergedClassNames from '../../utils/getMergedClassNames'
-import styles from './style.scss'
+import styles from './styles.scss'
 
 const cx = classNames.bind(styles)
 
