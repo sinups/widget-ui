381f5623a7902d02cef631702a8515fcb1924a80
webtheory@mail.ru <andromeda.060316698>
2020-03-22 14:57:05 +0200

    update launcher

--- src/components/Launcher/Launcher.js
+++ src/components/Launcher/Launcher.js
@@ -6,13 +6,67 @@ import styles from './styles.scss'
 import Icon from '../Icon'
 
 const cx = classNames.bind(styles)
-
 const baseClass = 'launcher'
-
 const acceptedSizes = ['small', 'regular', 'large']
+
 const Launcher = ({
-  size = 'large', open, ...restProps
+  size = 'large', open, pulsation, animation3D, iconSize, iconColor, ...restProps
 }) => {
+  const detectSize = () => {
+    if (typeof iconSize === 'number') return iconSize
+    if (iconSize === 'small') return 18
+    if (iconSize === 'medium') return 28
+    if (iconSize === 'large') return 42
+
+    return 24
+  }
+
+  function renderPulsation() {
+    const style = {
+      width: '100px',
+      height: '100px',
+    }
+
+    return <div className={styles.pulsation} style={style} />
+  }
+
+  function renderIcon() {
+    const iconSize = detectSize()
+    const style = {
+      width: iconSize,
+      height: iconSize,
+    }
+    if (open === false && animation3D === false) {
+      return (
+
+        <Icon
+          name={open ? 'close' : 'chat-icon'}
+          className={styles.icon}
+          size={iconSize}
+          color={iconColor}
+        />
+      )
+    }
+
+    // widget not open and 3D is on
+    if (!open && animation3D === true) {
+      return (
+        <div className={styles.flip_box} style={style}>
+          <div className={styles.flip_box_inner}>
+            <div className={styles.flip_box_front}>
+              <Icon name="chat-icon" className={styles.icon} color={iconColor} size={iconSize} />
+              {/* <img  className={styles.flip_box_front_img} style={style} src="https://picsum.photos/200/200/?${random(1, 20)}" alt=""/> */}
+            </div>
+            <div className={styles.flip_box_back}>
+              <Icon name="chat-icon" className={styles.icon} color={iconColor} size={iconSize} />
+            </div>
+          </div>
+        </div>
+      )
+    }
+    // widget openq
+    return <Icon name="close" className={styles.icon} color="#fff" size={iconSize} />
+  }
   const mergedClassNames = getMergedClassNames(
     cx({
       [baseClass]: true,
@@ -23,15 +77,8 @@ const Launcher = ({
   return (
     <div {...restProps} className={mergedClassNames}>
 
-      {open === true ? (
-        <div className={styles[`${baseClass}__close_icon`]}>
-          <Icon name="close" color="#fff" size={28} />
-        </div>
-      ):
-        <div className={styles[`${baseClass}__chat_icon`]}>
-          <Icon name="chat-icon" color="#fff" size={28} />
-        </div>
-      }
+      <span className={styles.fix}>{renderIcon()}</span>
+      {pulsation ? renderPulsation() : null}
     </div>
   )
 }
@@ -40,10 +87,19 @@ Launcher.propTypes = {
   size: PropTypes.oneOf(acceptedSizes),
   /** Управление open состоянием компонента */
   open: boolean,
+  pulsation: boolean,
+  animation3D: boolean,
+  iconSize: PropTypes.string,
+  iconColor: PropTypes.string,
 }
 
 Launcher.defaultProps = {
-  open: 'false',
+  open: false,
+  animation3D: false,
+  pulsation: false,
+  size: 'regular',
+  iconColor: '#fff',
+  iconSize: 'medium',
 }
 
 export default Launcher
--- src/components/Launcher/README.md
+++ src/components/Launcher/README.md
@@ -1,9 +1,13 @@
 #### Default Launcher
 ```jsx
-<Launcher></Launcher>
+<Launcher pulsation></Launcher>
 ```
 
 #### Open Launcher
 ```jsx
 <Launcher open></Launcher>
+```
+#### Pulsation and 3D animation  Launcher
+```jsx
+<Launcher animation3D></Launcher>
 ```
